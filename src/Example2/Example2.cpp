9aee044514599483deae77b41c4a40284eac711c
Michael Haberler <git@mah.priv.at>
2023-05-06 12:47:52 +0200

    add OTA Pull example

---
+++ src/Example2/Example2.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <Streaming.h>
+#include <Functor.h>
+
+const long BAUD = 115200;
+
+//Button is a component that provides a functor-based
+//callback mechanism, so you can wire it up to whatever you wish
+
+class Button
+{
+public:
+  //ctor takes a functor and stores it away in a member
+
+  Button(const Functor0 & uponClickDoThis) :
+  notify(uponClickDoThis)
+  {}
+  void click()
+  {
+    //invoke the functor, thus calling back client
+    notify();
+  }
+private:
+  //note this is a data member with a verb for a name - matches its
+  //function-like usage
+  Functor0 notify;
+};
+
+class CDPlayer
+{
+public:
+  void play()
+  {
+    Serial << "Playing" << endl;
+  }
+  void stop()
+  {
+    Serial << "Stopped" << endl;
+  }
+};
+
+void wow()
+{
+  Serial  << "Wow!" << endl;
+}
+
+void setup()
+{
+  Serial.begin(BAUD);
+  delay(1000);
+
+  // makeFunctor from object and ptr-to-member-function
+  CDPlayer myCD;
+  Button playButton(makeFunctor((Functor0 *)0,myCD,&CDPlayer::play));
+  Button stopButton(makeFunctor((Functor0 *)0,myCD,&CDPlayer::stop));
+  playButton.click(); //calls myCD.play()
+  stopButton.click();  //calls myCD.stop()
+
+  // makeFunctor from pointer-to-function
+  Button wowButton(makeFunctor((Functor0 *)0,&wow));
+  wowButton.click(); //calls wow()
+}
+
+void loop()
+{
+}
---
+++ src/OTA-Pull/ota-test.cpp
@@ -0,0 +1,136 @@
+// see  lib/ESP32-OTA-Pull/examples/Basic-OTA-Example/Basic-OTA-Example.ino
+
+
+#ifdef M5UNIFIED
+#include <M5Unified.h>
+#endif
+
+#include <Arduino.h>
+
+const char *CurrentVersion = VERSION;
+const char *jsonUrl = JSON_URL;
+// const char *board = BOARD;
+// const char *device = DEVICE;
+
+#include "ESP32OTAPull.h"
+
+const char *errtext(int code);
+void DisplayInfo();
+
+void callback(int offset, int totallength);
+
+void setup() {
+#ifdef M5UNIFIED
+  auto cfg = M5.config();
+  M5.begin(cfg);
+#else
+  Serial.begin(115200);
+#endif
+  while (!Serial) {
+    yield();
+  }
+
+#if defined(LED_BUILTIN)
+  pinMode(LED_BUILTIN, OUTPUT);
+#endif
+
+  DisplayInfo();
+
+  Serial.printf("Connecting to WiFi '%s'...", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (!WiFi.isConnected()) {
+    Serial.print(".");
+    delay(250);
+  }
+  Serial.printf("\n\n");
+
+  // First example: update should NOT occur, because Version string in JSON
+  // matches local VERSION value.
+  ESP32OTAPull ota;
+
+  // ota.OverrideBoard(board);
+  // ota.OverrideDevice(device);
+  // ota.AllowDowngrades(true);
+  
+  ota.SetCallback(callback);
+  Serial.printf(
+      "We are running version %s of the sketch, Board='%s', Device='%s'.\n",
+      CurrentVersion, ARDUINO_BOARD, WiFi.macAddress().c_str());
+  Serial.printf("Checking %s to see if an update is available...\n", jsonUrl);
+  int ret = ota.CheckForOTAUpdate(jsonUrl, CurrentVersion);
+  Serial.printf("CheckForOTAUpdate returned %d (%s)\n\n", ret, errtext(ret));
+
+  delay(3000);
+
+  // Second example: update *will* happen because we are pretending we have an
+  // earlier version
+  Serial.printf("But if we pretend like we're running version 0.0.0, we SHOULD "
+                "see an update happen.\n");
+  ret = ota.CheckForOTAUpdate(jsonUrl, "0.0.0");
+  Serial.printf("(If the update succeeds, the reboot should prevent us ever "
+                "getting here.)\n");
+  Serial.printf("CheckOTAForUpdate returned %d (%s)\n\n", ret, errtext(ret));
+}
+
+void loop() {}
+
+const char *errtext(int code) {
+  switch (code) {
+  case ESP32OTAPull::UPDATE_AVAILABLE:
+    return "An update is available but wasn't installed";
+  case ESP32OTAPull::NO_UPDATE_PROFILE_FOUND:
+    return "No profile matches";
+  case ESP32OTAPull::NO_UPDATE_AVAILABLE:
+    return "Profile matched, but update not applicable";
+  case ESP32OTAPull::UPDATE_OK:
+    return "An update was done, but no reboot";
+  case ESP32OTAPull::HTTP_FAILED:
+    return "HTTP GET failure";
+  case ESP32OTAPull::WRITE_ERROR:
+    return "Write error";
+  case ESP32OTAPull::JSON_PROBLEM:
+    return "Invalid JSON";
+  case ESP32OTAPull::OTA_UPDATE_FAIL:
+    return "Update fail (no OTA partition?)";
+  default:
+    if (code > 0)
+      return "Unexpected HTTP response code";
+    break;
+  }
+  return "Unknown error";
+}
+
+void DisplayInfo() {
+  char exampleImageURL[256];
+  snprintf(exampleImageURL, sizeof(exampleImageURL),
+           "https://example.com/Basic-OTA-Example-%s-%s.bin", ARDUINO_BOARD,
+           CurrentVersion);
+
+  Serial.printf("Basic-OTA-Example v%s\n", CurrentVersion);
+  Serial.printf("You need to post a JSON (text) file similar to this:\n");
+  Serial.printf("{\n");
+  Serial.printf("  \"Configurations\": [\n");
+  Serial.printf("    {\n");
+  Serial.printf("      \"Board\": \"%s\",\n", ARDUINO_BOARD);
+  Serial.printf("      \"Device\": \"%s\",\n", WiFi.macAddress().c_str());
+  Serial.printf("      \"Version\": %s,\n", CurrentVersion);
+  Serial.printf("      \"URL\": \"%s\"\n", exampleImageURL);
+  Serial.printf("    }\n");
+  Serial.printf("  ]\n");
+  Serial.printf("}\n");
+  Serial.printf("\n");
+  Serial.printf("(Board, Device, Config, and Version are all *optional*.)\n");
+  Serial.printf("\n");
+  Serial.printf("Post the JSON at, e.g., %s\n", jsonUrl);
+  Serial.printf("Post the compiled bin at, e.g., %s\n\n", exampleImageURL);
+}
+
+void callback(int offset, int totallength) {
+  Serial.printf("Updating %d of %d (%02d%%)...\n", offset, totallength,
+                100 * offset / totallength);
+#if defined(LED_BUILTIN) // flicker LED on update
+  static int status = LOW;
+  status = status == LOW && offset < totallength ? HIGH : LOW;
+  digitalWrite(LED_BUILTIN, status);
+#endif
+}
