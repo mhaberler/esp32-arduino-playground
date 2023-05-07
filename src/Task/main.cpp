c7dc9dc73ac9d1055a5e25ba8948d99109166898
Michael Haberler <git@mah.priv.at>
2023-05-07 15:35:41 +0200

    task example

---
+++ src/Task/main.cpp
@@ -0,0 +1,18 @@
+// example from https://github.com/pschatzmann/arduino-freertos-addons/tree/main
+
+#include "freertos-all.h"
+#include <Arduino.h>
+
+// Task task("name",1000,10, [](){ static size_t count=0;
+// queue.Enqueue(&(++count));});
+Task task(
+    "name", 1000, 10, []() { Serial.println("hi"); }, 5);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("starting Task:");
+  task.Start(0);
+}
+
+void loop() { delay(100); }
