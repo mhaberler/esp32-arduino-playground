da5f881b2d121acda21bd6de8fec835a504a398d
Michael Haberler <git@mah.priv.at>
2023-05-07 16:39:10 +0200

    CyclicTask: usage demo

--- src/Task/main.cpp
+++ src/Task/main.cpp
@@ -1,21 +1,39 @@
 // example from https://github.com/pschatzmann/arduino-freertos-addons/tree/main
 
-#include "CyclicTask.hpp"
 #include "freertos-all.h"
 #include <Arduino.h>
+#define TOGGLE(pin) digitalWrite(pin, !digitalRead(pin))
 
 CyclicTask task(
-    "repeat", 1000, 10, []() { Serial.println("hi"); });
+    "fast", 1000, 3, []() { TOGGLE(IMU_PIN); }, 10);
 CyclicTask task2(
-    "repeat2", 1000, 10, []() { Serial.printf("there\n"); });
+    "slow", 1000, 2, []() { TOGGLE(REPORT_PIN); }, 50);
 
 void setup() {
   int i = 4711;
   Serial.begin(115200);
   delay(1000);
-  Serial.println("starting Task:");
+
+  pinMode(ISR_PIN, OUTPUT);
+  digitalWrite(ISR_PIN, LOW);
+  pinMode(IMU_PIN, OUTPUT);
+  digitalWrite(IMU_PIN, LOW);
+  pinMode(REPORT_PIN, OUTPUT);
+  digitalWrite(REPORT_PIN, LOW);
+  pinMode(LOOP_PIN, OUTPUT);
+  digitalWrite(LOOP_PIN, LOW);
+
+  Serial.println("starting tasks:");
   task.Start(1);
   task2.Start(0);
+  delay(300);
+  TOGGLE(ISR_PIN);
+  task2.setRate(5);
 }
 
-void loop() { delay(100); }
+void loop() {
+  digitalWrite(LOOP_PIN, HIGH);
+  delay(100);
+  digitalWrite(LOOP_PIN, LOW);
+  delay(100);
+}
